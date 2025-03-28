4164d4d32007ea1dff63b95f4f18a3b81d3e92eb
Richard <codyrmill@gmail.com>
2025-03-28 18:01:41 -0400

    Creates a random integer, converts to binary, creates problem as string, sends string to setProblem method

--- JavaPorj.java
+++ JavaPorj.java
@@ -1,4 +1,12 @@
 import java.util.ArrayList;
+
+//The problems and their corresponding answer must be stored as an array or arraylist as ProblemAnsAnswer objects
+
+//Create B as a ProblemAndAnswer object, it's assigned default String Problem and String Answer, store B in Arraylist, have B's problem and answer
+// calculated and updated, continue with rest
+
+// Create number vars separate to the object, and assing it to object, we have object a, generate two numbers with their own variables, send to setProblem
+
 class ProblemAndAnswer{
         String problem;
         String answer;
@@ -31,19 +39,45 @@ class ProblemAndAnswer{
 
 public class JavaPorj {
     public static void main(String[] args) {
-        ArrayList<String> pandA = new ArrayList<>();
+        ArrayList<ProblemAndAnswer> pandA = new ArrayList<>(); // Arraylists that hold objects of the ProbelmAndAnswer class
+        ProblemAndAnswer major = new ProblemAndAnswer();
+        
+
 
         int a =  (int)(Math.random()*64); // generate random number between 0 and 63
-        String b = Integer.toBinaryString(a);
-        System.out.println(a);
-        System.out.println(b);
+        int b = (int)(Math.random()*64);
+        
+        
 
-        ProblemAndAnswer major = new ProblemAndAnswer();
-        major.setProblem(b);
+        while (b > a){ // Don't want negative numbers for this assigment
+             b = (int)(Math.random()*64);
+        }
+        String aBinary = Integer.toBinaryString(a);
+        String bBinary = Integer.toBinaryString(b);
+
+        String problem = aBinary + " + " + bBinary;
+
+
+
+
+        
+
+        
+        
+        
+
+        pandA.add(major); // Adds major object ot arraylist
+
+        
+
+        major.setProblem(problem);
+        System.out.println(major.problem);
     }
 
 
 
 
 }
+
+// Im thinking, make an array of 15 strings, loop the array in order to create 15 
 
